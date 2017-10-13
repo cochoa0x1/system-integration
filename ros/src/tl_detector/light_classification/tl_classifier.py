0cd51944c60461a42398d1d6b0ef6de4a3423963
chris <cochoa0x1@gmail.com>
2017-10-13 15:10:02 +0900

    it stops when it should! (kind)

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,14 @@
 from styx_msgs.msg import TrafficLight
+import matplotlib.pyplot as plt
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        
+        fig = plt.figure()
+        data = np.zeros((nx, ny))
+        self.im = plt.imshow(data, cmap='gist_gray_r', vmin=0, vmax=1)
+        plt.show()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
